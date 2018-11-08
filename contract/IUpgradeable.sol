fc9322de907dc7965ea537136a5c97327fc218a5
trifist <trifist0115@hotmail.com>
2018-11-08 11:10:02 +0800

    实现升级接口

--- contract/IUpgradeable.sol
+++ contract/IUpgradeable.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.4.25;
 
 interface IUpgradeable {
-    
+    function upgrade(address newImpl) external;
+
+    event Upgrade(
+        address indexed newAddress,
+        address indexed oldAddress
+    );
 }
--- contract/SalonToken.sol
+++ contract/SalonToken.sol
@@ -2,61 +2,74 @@ pragma solidity ^0.4.25;
 
 import "./Administrative.sol";
 import "./IExtendedERC20.sol";
+import "./SalonTokenStorage.sol";
+import "./IUpgradeable.sol";
 
-contract SalonToken is IExtendedERC20, Administrative {
-    IExtendedERC20 salonTokenImpl;
+contract SalonToken is IExtendedERC20, IUpgradeable, Administrative {
+    SalonTokenStorage tokenStorage;
+    address salonTokenImpl;
 
     modifier onlyPayloadSize(uint size) {
         require(msg.data.length >= size + 4);
         _;
     }
 
-    constructor(string name, string symbol, uint8 decimals, address implAddr) public {
-        salonTokenImpl = IExtendedERC20(implAddr);
-
+    constructor(string name, string symbol, uint8 decimals, address implAddr, address storageAddr) public {
+        tokenStorage = SalonTokenStorage(storageAddr);
+        salonTokenImpl = implAddr;
+        tokenStorage.setName(name);
+        tokenStorage.setSymbol(symbol);
+        tokenStorage.setDecimals(decimals);
     }
 
     function totalSupply() external view returns (uint256) {
-        return salonTokenImpl.totalSupply();
+        return tokenStorage.totalSupply();
     }
 
     function balanceOf(address who) external view returns (uint256) {
-        return salonTokenImpl.balanceOf(who);
+        return tokenStorage.balanceOf(who);
     }
 
     function allowance(address owner, address spender) external view returns (uint256) {
-        return salonTokenImpl.allowance(owner, spender);
+        return tokenStorage.allowance(owner, spender);
     }
 
     function transfer(address to, uint256 value) external onlyPayloadSize(2 * 32) returns (bool) {
-        return salonTokenImpl.transfer(to, value);
+        bytes4 methodId = bytes4(keccak256("transfer(address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, to, value);
     }
 
     function approve(address spender, uint256 value) external onlyPayloadSize(2 * 32) returns (bool) {
-        return salonTokenImpl.approve(spender, value);
+        bytes4 methodId = bytes4(keccak256("approve(address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, spender, value);
     }
 
     function transferFrom(address from, address to, uint256 value) external onlyPayloadSize(3 * 32) returns (bool) {
-        return salonTokenImpl.transferFrom(from, to, value);
+        bytes4 methodId = bytes4(keccak256("transferFrom(address,address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, from, to, value);
     }
 
     function increaseAllowance(address spender, uint256 addedValue) external onlyPayloadSize(2 * 32) returns (bool) {
-        return salonTokenImpl.increaseAllowance(spender, addedValue);
+        bytes4 methodId = bytes4(keccak256("increaseAllowance(address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, spender, addedValue);
     }
 
     function decreaseAllowance(address spender, uint256 subtractedValue) external onlyPayloadSize(2 * 32) returns (bool) {
-        return salonTokenImpl.decreaseAllowance(spender, subtractedValue);
+        bytes4 methodId = bytes4(keccak256("decreaseAllowance(address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, spender, subtractedValue);
     }
 
     function mint(address account, uint256 value) external onlyPrivileged returns (bool) {
-        return salonTokenImpl.mint(account, value);
+        bytes4 methodId = bytes4(keccak256("mint(address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, account, value);
     }
 
     function burn(address account, uint256 value) external onlyPrivileged returns (bool) {
-        return salonTokenImpl.burn(account, value);
+        bytes4 methodId = bytes4(keccak256("burn(address,uint256)"));
+        return salonTokenImpl.delegatecall(methodId, account, value);
     }
 
-    function upgrade(address newImpl) public onlyPrivileged {
-        salonTokenImpl = IExtendedERC20(newImpl);
+    function upgrade(address newImpl) external onlyPrivileged {
+        salonTokenImpl = newImpl;
     }
 }
