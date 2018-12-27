44176181a8a2657996e69c8232e8aa5292433118
chtian <chtian@anfengde.com>
2018-12-27 22:07:40 +0800

    out of gas in truffle unit test

--- contract/test/tokens/SalonToken.test.js
+++ contract/test/tokens/SalonToken.test.js
@@ -6,12 +6,12 @@ contract('SalonToken', (accounts) => {
   let token;
 
   beforeEach(async () => {
-    // token = await SalonToken.new('AnHui Blockchain Salon Token', 'AHBS', 18, {gas: 4712388});
+    token = await SalonToken.new('AnHui Blockchain Salon Token', 'AHBS', 18, {gas: 6500000});
   });
 
   it('get name test', async () => {
-    // let name = await token.name.call();
-    // assert.equal(name, 'AnHui Blockchain Salon Token');
+    let name = await token.name.call();
+    assert.equal(name, 'AnHui Blockchain Salon Token');
 
     // let symbol = await token.symbol.call();
     // assert.equal(symbol, 'AHBS');
